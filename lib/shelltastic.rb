369ce7d47b465793c3446c7e8d4e87da74e6b623
Bradley Smith <bradleydsmith@gmail.com>
2013-02-03 17:32:57 -0500

    Bump version 0.2.2. Move timer object out of IO#popen; inject object insteadMove #string_nil_or_blank? to utils module.  Now extend this module in IO.

--- lib/shelltastic.rb
+++ lib/shelltastic.rb
@@ -1,8 +1,8 @@
 require_relative "shelltastic/version"
+require_relative "shelltastic/utils"
 require_relative "shelltastic/command_io"
 require_relative "shelltastic/timer"
 require_relative "shelltastic/exceptions"
-require 'open4'
 
 # @! ShellTastic 
 module ShellTastic
@@ -13,10 +13,11 @@ module ShellTastic
     class << self
       # encapsualtes the popen call 
       # @param [String] command the command that wants to be run
+      # @param [Object] timer timer object, @see ShellTastic::Timer
       # @param [Array] args an array of other arguments
       # @return [Hash] from #::IO::popen
       def run(command, *args)
-        ShellTastic::IO.popen(command)
+        ShellTastic::IO.popen(command, ShellTastic::Timer)
       end
     end
   end
--- lib/shelltastic/command_io.rb
+++ lib/shelltastic/command_io.rb
@@ -1,35 +1,30 @@
+# encoding: utf-8
+
+require 'open4'
+
 module ShellTastic
   module IO
+    extend ShellTastic::Utils
     class << self
-      # A dirty string check for nil empty or just whitespace
-      # @param [String] str the string the needs to be checked
-      # @return [Boolean, nil] true if str is empty, nil, or just whitespace
-      def string_nil_or_blank? str
-        # raise if command is empty or nil
-        if str !~ /[^[:space:]]/ || str.nil? || str.empty?
-          raise ShellTastic::CommandException.new("Command is emtpy or nil")
-        end
-      end
-
       # A wrapper around popen4
-      # This is called from the run method
-      # @see #run
+      #
+      # @see #Command.run
       # @param [String] command the command to run in the shell
       # @return [Hash] command_results the command results from shell
       # { :output, :pid, :error, :start, :stop, :total_time, :exitstatus }
-      def popen command
-        string_nil_or_blank? command
+      def popen(command, timer=ShellTastic::Timer)
+        string_nil_or_blank?(command)
         command_results = {}
         begin
-          start = ShellTastic::Timer.start
-          return_code = Open4::popen4(command) do |pid, stdin, stdout, stderr|
+          start = timer.start
+          return_code = Open4::spawn(command) do |pid, stdin, stdout, stderr|
             command_results.store(:output,stdout.read.strip)
             command_results.store(:pid,pid)
             command_results.store(:error,stderr.read.strip)
             stdin.close
           end
-          stop = ShellTastic::Timer.stop
-          total_time = ShellTastic::Timer.total_time
+          stop = timer.stop
+          total_time = timer.total_time
           command_results.merge!(start: start, stop: stop, 
                                  total_time: total_time, 
                                  exitstatus: return_code.exitstatus)
